900f9dc22f761bf26e62d2a37e022c918e1842d1
Artur Paschoal <128155308+paschoal-artur@users.noreply.github.com>
2023-04-19 19:42:15 -0300

    code final

--- exe3.py
+++ exe3.py
@@ -6,14 +6,16 @@ money = input("How much money do you receive? \n")
 money = int(money)
 sex = input("What's your sex? M or F \n")
 civil_state = input("What's you civil state? S, C, V, D \n")
+
 confirm = False
+
 while confirm == False:
-    if len(name) < 3 :
+    while len(name) < 3 :
         name = input("Type your name: \n")
-    if age < 0 or age > 150 :
+    while age < 0 or age > 150 :
         age = int(input("Type your age: \n"))
         age = int(age)
-    if money < 0 :
+    while money < 0 :
         money = int(input("How much money do you receive? \n"))
         money = int(money)
     print("All okay!")
